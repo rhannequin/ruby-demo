14bfec2ffbfa2516b9859059313c8fa84ce52bff
rhannequin <remy.hannequin@gmail.com>
2013-06-05 09:01:12 +0200

    Add 'edit' and 'new' template pages

--- app.rb
+++ app.rb
@@ -9,6 +9,7 @@ require 'mongoid'
 # MongoDB config
 Mongoid.load! './mongoid.yml'
 
+# SASS Helpers
 helpers do
   def sass(template, *args)
     template = "#{settings.sass_dir}/#{template}".to_sym if template.is_a? Symbol
@@ -16,25 +17,35 @@ helpers do
   end
 end
 
+# Sinatra conf
 configure do
   set :sass_dir, File.join('..', 'public', 'assets', 'stylesheets')
 end
 
+# Handle SASS stylesheets
 get '/assets/stylesheets/:name.css' do
   content_type 'text/css', :charset => 'utf-8'
   sass params[:name].to_sym
 end
 
 
+# Root page
 get '/' do
   haml :index, :locals => {:title => "Kon’nichi wa, Ruby"}
 end
 
+# List of Pokemons
 get '/pokedex' do
   pokemons = Pokemon.all.to_a
   haml :'pokedex/list', :locals => {:title => 'Pokedex', :pokemons => pokemons}
 end
 
+# Add a Pokemon
+get '/pokedex/new' do
+  haml :'pokedex/new', :locals => {:title => "Add a Pokemon"}
+end
+
+# Show a Pokemon
 get '/pokedex/:id' do
   pokemon_id = params[:id]
   begin
@@ -44,6 +55,20 @@ get '/pokedex/:id' do
   end
   haml :'pokedex/show', :locals => {:title => pokemon.name, :pokemon => pokemon}
 end
+
+# Edit a Pokemon
+get '/pokedex/:id/edit' do
+  pokemon_id = params[:id]
+  begin
+    pokemon = Pokemon.find(pokemon_id)
+  rescue
+    return 404
+  end
+  haml :'pokedex/edit', :locals => {:title => "Edit #{pokemon.name}", :pokemon => pokemon}
+end
+
+
+# Models
 
 class Pokemon
   include Mongoid::Document
